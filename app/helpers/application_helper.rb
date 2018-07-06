23218336b38fd0f0ca71892fcff6c83de7f06f57
Aleksandr Kojemyako <alekskojem@gmail.com>
2018-07-06 14:57:58 +0300

    Add helper for head

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+ 
+  def full_title(page_title)
+    <title><%= full_title(yield(:title)) %></title>
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
